37ef568181e573a535c347478da97e7f9a3acc8e
duru100470 <zenpia@naver.com>
2023-06-09 03:47:21 +0900

    Token 검사 구현

---
+++ ZDrive-Test/Services/SessionTokenAuthenticationSchemeHandlerTest.cs
@@ -0,0 +1,130 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using ZDrive.Services;
+
+namespace ZDrive_Test;
+
+public class SessionTokenAuthenticationSchemeHandlerTest
+{
+    private Mock<IOptionsMonitor<SessionTokenAuthenticationSchemeOptions>> _options = null!;
+    private Mock<ILoggerFactory> _loggerFactory = null!;
+    private Mock<UrlEncoder> _encoder = null!;
+    private Mock<ISystemClock> _clock = null!;
+    private Mock<ISessionStorage> _sessionStorage = null!;
+    private SessionTokenAuthenticationSchemeHandler _handler = null!;
+
+    private SessionTokenAuthenticationSchemeHandler GetHandler()
+        => new SessionTokenAuthenticationSchemeHandler(_options.Object, _loggerFactory.Object, _encoder.Object, _clock.Object, _sessionStorage.Object);
+
+    [SetUp]
+    public void SetUp()
+    {
+        _options = new Mock<IOptionsMonitor<SessionTokenAuthenticationSchemeOptions>>();
+            
+        // This Setup is required for .NET Core 3.1 onwards.
+        _options
+            .Setup(x => x.Get(It.IsAny<string>()))
+            .Returns(new SessionTokenAuthenticationSchemeOptions());
+        
+        var logger = new Mock<ILogger<SessionTokenAuthenticationSchemeHandler>>();
+        _loggerFactory = new Mock<ILoggerFactory>();
+        _loggerFactory.Setup(x => x.CreateLogger(It.IsAny<String>())).Returns(logger.Object);
+
+        _encoder = new Mock<UrlEncoder>();
+        _clock = new Mock<ISystemClock>();
+        _sessionStorage = new Mock<ISessionStorage>();
+    }
+
+
+    [Test]
+    public async Task HandleAuthenticateAsync_NoSSIDCookie_ReturnsAuthenticateResultFail()
+    {
+        // Arrange
+        var context = new DefaultHttpContext();
+        _handler = GetHandler();
+
+        // Act
+        await _handler.InitializeAsync(new AuthenticationScheme("SessionTokens", null, typeof(SessionTokenAuthenticationSchemeHandler)), context);
+        var result = await _handler.AuthenticateAsync();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Succeeded, Is.False);
+        Assert.That(result.Failure?.Message, Is.EqualTo("Session authentication failed.  Authorization header is missing."));
+    }
+
+    [Test]
+    public async Task HandleAuthenticateAsync_InvalidSSID_ReturnsAuthenticateResultFail()
+    {
+        // Arrange
+        var context = new Mock<HttpContext>();
+        context.Setup(e => e.Request.Cookies["sessionId"])
+            .Returns("invalidSSID");
+        _handler = GetHandler();
+
+        // Act
+        await _handler.InitializeAsync(new AuthenticationScheme("SessionTokens", null, typeof(SessionTokenAuthenticationSchemeHandler)), context.Object);
+        var result = await _handler.AuthenticateAsync();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Succeeded, Is.False);
+        Assert.That(result.Failure?.Message, Is.EqualTo("Session authentication failed.  Token is invalid."));
+    }
+
+    [Test]
+    public async Task HandleAuthenticateAsync_NonSSID_ReturnsAuthenticateResultFail()
+    {
+        // Arrange
+        var ssid = Guid.NewGuid();
+        var session = new Session(1, DateTime.Now);
+        var context = new Mock<HttpContext>();
+        context.Setup(e => e.Request.Cookies["sessionId"])
+            .Returns(ssid.ToString());
+
+        _sessionStorage.Setup(e => e.TryGetUser(It.IsAny<Guid>(), out session))
+            .Returns(false);
+
+        _handler = GetHandler();
+
+        // Act
+        await _handler.InitializeAsync(new AuthenticationScheme("SessionTokens", null, typeof(SessionTokenAuthenticationSchemeHandler)), context.Object);
+        var result = await _handler.AuthenticateAsync();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Succeeded, Is.False);
+        Assert.That(result.Failure?.Message, Is.EqualTo("Session authentication failed.  Token does not exist in storage."));
+    }
+
+    [Test]
+    public async Task HandleAuthenticateAsync_ValidToken_ReturnsAuthenticateResultSuccessAsync()
+    {
+        // Arrange
+        var ssid = Guid.NewGuid();
+        var session = new Session(1, DateTime.Now);
+        var context = new Mock<HttpContext>();
+        context.Setup(e => e.Request.Cookies["sessionId"])
+            .Returns(ssid.ToString());
+
+        _sessionStorage.Setup(e => e.TryGetUser(It.Is<Guid>(e => e != ssid), out session))
+            .Returns(false);
+        _sessionStorage.Setup(e => e.TryGetUser(ssid, out session))
+            .Returns(true);
+
+        _handler = GetHandler();
+
+        // Act
+        await _handler.InitializeAsync(new AuthenticationScheme("SessionTokens", null, typeof(SessionTokenAuthenticationSchemeHandler)), context.Object);
+        var result = await _handler.AuthenticateAsync();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Succeeded, Is.True);
+        Assert.That(result.Ticket?.Principal.HasClaim(c => c.Type == "Id" && c.Value == "1"), Is.True);
+    }
+}
--- ZDrive/Services/Auth/SessionTokenAuthenticationSchemeHandler.cs
+++ ZDrive/Services/Auth/SessionTokenAuthenticationSchemeHandler.cs
@@ -6,28 +6,31 @@ using Microsoft.Extensions.Options;
 namespace ZDrive.Services;
 public class SessionTokenAuthenticationSchemeHandler : AuthenticationHandler<SessionTokenAuthenticationSchemeOptions>
 {
+    private readonly ISessionStorage _session;
+
     public SessionTokenAuthenticationSchemeHandler(
         IOptionsMonitor<SessionTokenAuthenticationSchemeOptions> options,
         ILoggerFactory logger,
         UrlEncoder encoder,
-        ISystemClock clock) : base(options, logger, encoder, clock)
+        ISystemClock clock,
+        ISessionStorage session) : base(options, logger, encoder, clock)
     {
+        _session = session;
     }
 
     protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
     {
-        // Read the token from request headers/cookies
-        // Check that it's a valid session, depending on your implementation
 
-        Console.WriteLine("Test");
-        
-        // If the session is valid, return success:
-        var claims = new[] { new Claim(ClaimTypes.Name, "Test") };
+        if (Request.Cookies["sessionId"] == null)
+            return AuthenticateResult.Fail("Session authentication failed.  Authorization header is missing.");
+        if (!Guid.TryParse(Request.Cookies["sessionId"], out var guid))
+            return AuthenticateResult.Fail("Session authentication failed.  Token is invalid.");
+        if (!_session.TryGetUser(guid, out var session))
+            return AuthenticateResult.Fail("Session authentication failed.  Token does not exist in storage.");
+
+        var claims = new[] { new Claim("Id", session.Id.ToString()) };
         var principal = new ClaimsPrincipal(new ClaimsIdentity(claims, "Tokens"));
         var ticket = new AuthenticationTicket(principal, this.Scheme.Name);
         return AuthenticateResult.Success(ticket);
-
-        // If the token is missing or the session is invalid, return failure:
-        // return AuthenticateResult.Fail("Authentication failed");
     }
 }
