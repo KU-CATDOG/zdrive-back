be7dce9a16bf004b35915cf79fe510d9f55d5f0c
duru100470 <zenpia@naver.com>
2023-06-11 20:03:31 +0900

    #5 CORS 문제 해결

--- ZDrive/Program.cs
+++ ZDrive/Program.cs
@@ -5,8 +5,8 @@ using ZDrive.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +24,18 @@ builder.Services.AddAuthentication()
     );
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine("CORS: AllowAnyOrigin");
+    app.UseCors(options =>
+    {
+        options.AllowAnyOrigin();
+        options.AllowAnyHeader();
+        options.AllowAnyMethod();
+    });
+}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
