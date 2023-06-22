d6c5f34c1c0943af14df72bfea34f48da60e7b29
duru100470 <zenpia@naver.com>
2023-06-22 19:46:09 +0900

    유저 참여 프로젝트 리스트, 유저 정보 업데이트 API

---
+++ ZDrive/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZDrive.Data;
+using ZDrive.Models;
+
+namespace ZDrive.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ZDriveDbContext _context;
+
+    public UserController(ZDriveDbContext context)
+    {
+        _context = context;
+    }
+
+    [Route("project")]
+    [HttpGet]
+    public async Task<IResult> ReadProjects()
+    {
+        var projects = from p in _context.Projects
+                       select p;
+
+        var sid = User.FindFirstValue(ClaimTypes.Sid);
+        if (sid != null)
+        {
+            projects = projects
+                .Where(p => p.UserId == int.Parse(sid));
+        }
+
+        var ret = await projects.ToListAsync();
+        return ret.Count > 0 ? Results.Ok(ret) : Results.NotFound();
+    }
+
+    [HttpPut]
+    public async Task<IResult> Update(UserInfo user)
+    {
+        var sid = User.FindFirstValue(ClaimTypes.Sid);
+        if (sid == null) return Results.Unauthorized();
+
+        var _user = await _context.Users.FindAsync(int.Parse(sid));
+        if (_user is null) return Results.NotFound();
+
+        if (_user.Name != user.Name)
+        {
+            var _studentNum = await _context.StudentNums.FindAsync(_user.StudentNumber);
+            if (_studentNum is null) return Results.NotFound("StudentNumber was not found.");
+
+            _user.Name = user.Name;
+            _studentNum.Name = user.Name;
+        }
+
+        await _context.SaveChangesAsync();
+        return Results.Created($"/calendar/{_user.Id}", user);
+    }
+}
